952072d5209610b221135a17c6a5884c6ac48f5c
= <125404099+afan2k0@users.noreply.github.com>
2023-10-08 23:29:21 -0400

    added classification

--- Parser/parse.py
+++ Parser/parse.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def prune_duplicates(input_file, output_file):
     # Load the data from the JSON file
@@ -9,6 +10,13 @@ def prune_duplicates(input_file, output_file):
     pruned_data = {}
     for item in data:
         place_id = item.get('place_id')
+        compound_code = item.get('plus_code', {}).get('compound_code', '')
+
+        # Check if the compound_code contains "New Brunswick"
+        if not re.search(r'new brunswick', compound_code, re.IGNORECASE):
+            print(f"Skipped business: {item['name']} with compound_code: {compound_code}")
+            continue
+
         if place_id:
             # If the place_id already exists in pruned_data, it's a duplicate
             if place_id in pruned_data:
@@ -28,6 +36,6 @@ def prune_duplicates(input_file, output_file):
     print(f"Pruned data written to {output_file}")
 
 # Usage
-input_filename = 'Parser\outputs.json'
-output_filename = 'Parser\pruned_outputs.json'
+input_filename = 'maps-test\Parser\outputs.json'
+output_filename = 'maps-test\Parser\pruned_outputs.json'
 prune_duplicates(input_filename, output_filename)
